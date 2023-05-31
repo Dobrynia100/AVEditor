d25de4f5d0f89c96abe6775fca8e40b982c2a341
Dobrynia100 <dobrynya2001@gmail.com>
2023-05-31 19:36:05 +0300

    fix

--- Editor.py
+++ Editor.py
@@ -1,23 +1,23 @@
 from pytube import YouTube
-from sys import argv
 import os
-print("download video(1) or audio(2)?")
-link =argv[1]
+from moviepy.editor import *
+
+print("Insert link to a video from Youtube")
+link = input()
 yt = YouTube(link)
+print("Title:",  yt.title)
+inp = input("download video(1) or audio(2)?")
+if float(inp)!= float(2):
+    downl=yt.streams.get_highest_resolution()
+    print('Video')
+else:
+    downl=yt.streams.get_audio_only()
+    print('Audio')
 
-print("Title:",yt.title)
-while():
-    if input() == 1:
-        downl=yt.streams.get_highest_resolution()
-        break
-    if input()==2:
-        downl=yt.streams.get_audio_only()
-        break
-    else:
-        print("wrong button")
 
-path=input("Input path to download")
+path = input("Input path to download\n")
 if os.path.isdir(path):
     downl.download(path)
+    print('Downloaded')
 else:
     print("The entered string is not a valid folder path or the folder does not exist")
